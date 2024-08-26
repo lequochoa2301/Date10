c57f0858dc7f8df98cb7a789c668794f2acfc1ab
lequochoa2301 <leduchoa23012002@gmail.com>
2024-08-26 20:19:59 +0700

    date

---
+++ src/main/java/com/example/day/entity/User.java
@@ -0,0 +1,57 @@
+package com.example.day.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "Name is mandatory") String getName() {
+        return name;
+    }
+
+    public void setName(@NotBlank(message = "Name is mandatory") String name) {
+        this.name = name;
+    }
+
+    public @NotBlank(message = "Email is mandatory") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "Email is mandatory") String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/day/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.day.repository;
+
+import com.example.day.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
